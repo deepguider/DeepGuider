8c77fdad01772b5ac3f47fa4df66967f1c2785c1
ececyh <ececyh@snu.ac.kr>
2019-10-06 00:17:51 +0900

    exploration module

---
+++ src/exploration/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from active_navigation import ActiveNavigationModule
+
+# anm = ActiveNavigationModule(map_manager)
+
+# anm.encodeVisualMemory(img, guidance, topometric_pose)
+
+# anm.calcExplorationGuidance(state, img)
+
+# if anm.isRecoveryGuidanceEnabled():
+# 	return anm.getRecoveryGuidance()
+
+# if anm.isExplorationGuidanceEnabled():
+# 	return anm.getExplorationGuidance()
+	
+# anm.calcNeedForOptimalViewpointGuidance(topometric_pose_conf, poi_conf, entrance, entrance_conf, poi_successes)
+
+# if anm.isOptimalViewpointGuidanceEnabled():
+# 	return anm.getOptimalViewpointGuidance()
