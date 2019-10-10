11af812341386a4a86c603482c92f2f5d6053229
ccsmm78 <ccsmm@etri.re.kr>
2019-10-10 16:54:42 +0900

    [VPS] minor

--- src/vps/netvlad/3run_test_pitt256k.sh
+++ src/vps/netvlad/3run_test_pitt256k.sh
@@ -1,7 +1,7 @@
 export TMPDIR="/tmp"
 export CHKDIR="./checkpoint"
 #export PRETRAINED="pretrained_checkpoint/vgg16_netvlad_checkpoint"
-export PRETRAINED="pretrained_checkpointi/vgg16_netvlad_checkpoint_gpu4"
+export PRETRAINED="pretrained_checkpoint/vgg16_netvlad_checkpoint_gpu4"
 
 # Dataset is hardcoded at pittsburgh.py as follows : "./netvlad_v100_datasets" directory
 # --dataPath : Path for centroid data.
