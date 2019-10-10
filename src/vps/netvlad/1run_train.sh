3299dc91d2f7312bb5182d5eb31dde64c546e8df
ccsmm78 <ccsmm@etri.re.kr>
2019-10-10 16:43:25 +0900

    [VPS] Modify automatic setup script "0set_env.sh". You can download pretrained DB using this script. You can also test using pretrained DB
    Do do : I have to add resume option with which one can
     train NN from a pretrained weight rather than very inital point.

--- src/vps/netvlad/1run_train.sh
+++ src/vps/netvlad/1run_train.sh
@@ -1,7 +1,5 @@
 export TMPDIR="/tmp"
 export CHKDIR="./checkpoint"
-#export PRETRAINED="pretrained_checkpoint/vgg16_netvlad_checkpoint"
-export PRETRAINED="pretrained_checkpointi/vgg16_netvlad_checkpoint_gpu4"
 
 # Dataset is hardcoded at pittsburgh.py as follows : "./netvlad_v100_datasets" directory
 # --dataPath : Path for centroid data.
@@ -19,12 +17,10 @@ export PRETRAINED="pretrained_checkpointi/vgg16_netvlad_checkpoint_gpu4"
 
 
 python main.py --mode=cluster --arch=vgg16 --pooling=netvlad --num_clusters=64 \
-				--resume=${PRETRAINED} \
 				--dataPath=${CHKDIR}/data --runsPath=${CHKDIR}/runs \
 				--nGPU 4
 
 
 python main.py --mode=train --arch=vgg16 --pooling=netvlad --num_clusters=64 \
-				--resume=${PRETRAINED} \
 				--dataPath=${CHKDIR}/data --runsPath=${CHKDIR}/runs \
 				--nGPU 4
