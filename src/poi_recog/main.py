c275d27fb9b9bc9fd0b36f0453956032dc8ae7cf
mj5834 <minjung.kim1994@gmail.com>
2019-10-27 17:50:42 +0900

    [poi_recog] main.py

--- src/poi_recog/main.py
+++ src/poi_recog/main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
+import pickle
 
 from PIL import Image
 from PIL import ImageFile
@@ -16,7 +17,7 @@ from logos import detect_logo, match_logo, detect_logo_demo
 #from similarity import load_brands_compute_cutoffs
 from utils import load_extractor_model, load_features, model_flavor_from_name, parse_input
 
-sim_threshold = 0.95
+sim_threshold = 0.80
 output_txt = 'out.txt'
 
 
@@ -48,8 +49,12 @@ def test(filename, timestamp):
     images = [ p for p in os.listdir(os.path.join(test_dir, 'input/'))] #if p.endswith('.jpg')]
     images_path = [ os.path.join(test_dir, 'input/', p) for p in images]
 
+    with open('./data/preprocessed/input_paths.pkl', 'r') as f:
+        input_paths = pickle.load(f)
+    (img_input, feat_input, sim_cutoff, (bins, cdf_list)) = load_brands_compute_cutoffs(
+                                input_paths, (model, my_preprocess), features, sim_threshold)
+
     start = timer()
-    times_list = []
     img_size_list = []
     candidate_len_list = []
     for i, img_path in enumerate(images_path):
@@ -60,9 +65,9 @@ def test(filename, timestamp):
                                           save_img_path = test_dir, postfix='_logo')
 
         ## match candidate logos to input
-        #outtxt, times = match_logo(image, prediction, (model, my_preprocess),
-        #        outtxt, (feat_input, sim_cutoff, bins, cdf_list, input_labels),
-        #        save_img = save_img_match, save_img_path=test_dir, timing=True)
+        logo_txt = match_logo(image, prediction, (model, my_preprocess),
+                 outtxt, (feat_input, sim_cutoff, bins, cdf_list, input_labels),
+                 save_img = True, save_img_path=test_dir, timing=True)
 
         img_size_list.append(np.sqrt(np.prod(image.size)))
         candidate_len_list.append(len(prediction))
