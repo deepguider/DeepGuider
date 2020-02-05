fc0a6af609552b4f5593b903ce06a109b7b15a0d
ccsmm78 <ccsmm@etri.re.kr>
2020-02-05 16:12:24 +0900

    [VPS] minor

--- run_vps.sh
+++ run_vps.sh
@@ -8,11 +8,11 @@ mkdir -p build
 cd build
 cmake ..
 make install
+
+echo "####### Run vps_test ..... #######"
 cd "$topdir/bin"
 ln -fs ../src/vps/netvlad/ccsmmutils/img_utils.py
 ln -fs ../src/vps/netvlad
 ln -fs ../src/vps/netvlad_etri_datasets
-
-echo "####### Run vps_test ..... #######"
 source ~/.virtualenvs/dg_venv3.6/bin/activate
 ./vps_test
