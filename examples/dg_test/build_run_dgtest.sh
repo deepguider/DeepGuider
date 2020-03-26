1414238a7b1f25b22b9a9f3fb41b1758faca9d75
roricljy <roricljy@gmail.com>
2020-03-26 10:03:08 +0900

    [dg_test] update shell script _build_run_dgtest.sh_

--- examples/dg_test/build_run_dgtest.sh
+++ examples/dg_test/build_run_dgtest.sh
@@ -6,15 +6,20 @@ cd build
 cmake ..
 make install
 if [ $? -ne 0 ];then # If it met error in previous step
-#echo $?
-cd ..
-exit 0
+    #echo $?
+    cd ..
+    exit 0
 fi
 
 echo "####### Run ..... #######"
 cd ../../../bin
 ln -sf ../src/vps/data_vps
 ln -sf ../src/poi_recog/model
+cd data
+ln -sf ../../src/poi_recog/data/test
+ln -sf ../../src/poi_recog/data/preprocessed
+ln -sf ../../src/poi_recog/data/litw_annotation.py
+cd ..
 
 #gdb ./dg_test #for debugging, you need to insert '-g' option at CXX_FLAGS in your CMakeLists.txt # in your CMakeLists.txt : set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -g")
 #kdbg ./dg_test #for debugging in graphic mode
