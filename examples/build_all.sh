3746873f9b138703af52a970f7276465bc788439
ccsmm78 <ccsmm@etri.re.kr>
2020-03-27 20:15:05 +0900

    [vps_test] bug that I have to run 'cmake ..' twice for avoding 'ld error' I solve the problem to move project() to the very begegging of the CMakeLists.txt as follows : 	cmake_minimum_required(VERSION 2.8) 	project(${ProjectId} C CXX)

--- examples/build_all.sh
+++ examples/build_all.sh
@@ -17,7 +17,7 @@ function build_script(){
 	found_script=`ls *build*.sh 2> /dev/null|grep -v 'run'`
 	if [ ! -z "$found_script" ];then
 		echo "[...] Run $D/$found_script"
-		./$found_script
+		bash ./$found_script
 		eval "$2=$found_script" # return value via second args
 	else
 		echo "[...] Run default build script because there is no build_script in $D"
@@ -34,7 +34,7 @@ function setup_script(){
 	if [ ! -z "$found_script" ];then
 		if [ ! -f "./$setup_switch" ];then
 			echo "[...] Run $D/$found_script"
-			./$found_script
+			bash ./$found_script
 			if [ $? -ne 0 ];then # If it met error in previous step
 			    echo $?
 			    exit 0
@@ -45,6 +45,7 @@ function setup_script(){
 	cd ..
 }
 
+## Current directory is examples
 counter=0
 for D in $(find . -mindepth 1 -maxdepth 1 -type d)
 do
