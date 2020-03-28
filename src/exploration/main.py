d171c6976b0da4aee7af771133b1be1faad6cff8
blackfoot <blackfoot@snu.ac.kr>
2020-03-28 10:03:33 +0900

    Add optimal viewpoint estimation

--- src/exploration/main.py
+++ src/exploration/main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 
 from src.exploration.active_navigation import ActiveNavigationModule
+import argparse
+import src.exploration.ov_utils.file_utils as file_utils
+import numpy as np
+from src.exploration.ov_utils.navi_data import Navi
 
-anm = ActiveNavigationModule(map_manager)
+parser = argparse.ArgumentParser(description='Optimal viewpoint estimation')
+parser.add_argument('--data_folder', default='./data/optimal_viewpoint/', type=str, help='folder path to input images')
+parser.add_argument('--central_guidance', default=True, type=bool, help='A guidance to make a robot to rotate for making PoI on the center of the view')
+parser.add_argument('--optimal_guidance', default=True, type=bool, help='A guidance for optimal viewpoint')
+parser.add_argument('--enable_ove', default=True, type=bool, help='enable optimal viewpoint estimator')
+parser.add_argument('--verbose', default=False, type=bool, help='verbose')
+args = parser.parse_args()
+NV = Navi()
+anm = ActiveNavigationModule(args, NV)
+
 
 # anm.encodeVisualMemory(img, guidance, topometric_pose)
 
@@ -16,6 +29,32 @@ anm = ActiveNavigationModule(map_manager)
     
 # anm.calcNeedForOptimalViewpointGuidance(topometric_pose_conf, poi_conf, entrance, entrance_conf, poi_successes)
 
+
 if anm.isOptimalViewpointGuidanceEnabled():
-    guidance = anm.getOptimalViewpointGuidance()
-    print(guidance)
+    image_list, sf_list, bbox_list, depth_list = file_utils.get_files(args.data_folder)
+    im_paths, target_pois = file_utils.get_annos(args.data_folder + 'anno/')
+    im_cnt = 0
+    tot_acc = []
+    tot_test = len(im_paths)
+    for im_path, target_poi in zip(im_paths, target_pois):
+        im_cnt += 1
+        file = im_path
+        init_view = file
+        NV.file2curpos(file)
+        print("Testing... {}/{}, Target: {}".format(im_cnt, tot_test, target_poi))
+        gt_view = file_utils.get_gt_templates(target_poi)
+        anm.calcOptimalViewpointGuidance(im_path, target_poi)
+
+        c_guidance = anm.getCentralViewpointGuidance()
+        guidance = anm.getOptimalViewpointGuidance()
+        final_view = anm.getOptimalViewpointPath()
+        if final_view != None:
+            print("Central guidance [rot]): ", c_guidance[-1])
+            print("Optimal guidance [disp_x, disp_y, rot]: [%.2fm, %.2fm, %d]" % (guidance[0], guidance[1], guidance[2]))
+            if NV.dist(final_view, init_view) != 0:
+                acc = 1 if NV.dist(final_view, gt_view) < 5 else 0
+                tot_acc.append(acc)
+        else:
+            print(">> No Guidance (Reasons: No detection boxes found, No nearby POI, detection boxes are found on the street)")
+
+    print("Final acc: ", np.mean(tot_acc))
---
+++ src/exploration/ov_utils/myutils.py
@@ -0,0 +1,139 @@
+import numpy as np
+import cv2
+import argparse
+import imutils
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+from sklearn.metrics.pairwise import cosine_similarity
+import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+from torchvision import models
+from torch.autograd import Variable
+import matplotlib.image as mpimg
+import src.exploration.ov_utils.imgproc as imgproc
+
+model = models.resnet18(pretrained=True).cuda()
+model.eval()
+scaler = transforms.Scale((224, 224))
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+to_tensor = transforms.ToTensor()
+def make_mask(bbox, shape):
+    x1, y1, x2, y2, x3, y3, x4, y4 = np.reshape(bbox, [-1])
+    mask = np.ones(shape)
+    a1 = -(y1 - y4) / (x1 - x4+0.0001)
+    b1 = -(y1 - (y1 - y4) / (x1 - x4+0.0001) * x1)
+    a2 = -(y1 - y2) / (x1 - x2+0.0001)
+    b2 = -(y1 - (y1 - y2) / (x1 - x2+0.0001) * x1)
+    a3 = -(y2 - y3) / (x2 - x3+0.0001)
+    b3 = -(y2 - (y2 - y3) / (x2 - x3+0.0001) * x2)
+    a4 = -(y3 - y4) / (x3 - x4+0.0001)
+    b4 = -(y3 - (y3 - y4) / (x3 - x4+0.0001) * x3)
+    for i in range(shape[1]):
+        for j in range(shape[0]):
+            if a1 > 0:
+                if (j < -(a1 * i + b1)):
+                    mask[j, i] = 0
+            else:
+                if (j > -(a1 * i + b1)):
+                    mask[j, i] = 0
+            if (j < -(a2 * i + b2)):
+                mask[j, i] = 0
+            if a3 > 0:
+                if (j > -(a3 * i + b3)):
+                    mask[j, i] = 0
+            else:
+                if (j < -(a3 * i + b3)):
+                    mask[j, i] = 0
+            if (j > -(a4 * i + b4)):
+                mask[j, i] = 0
+    return mask
+
+
+def template_matching(img_gray, bbox, templates):
+    template_matched = False
+    sizes = np.zeros((len(templates), len(bbox)))
+    for i in range(len(templates)):
+        for j in range(len(bbox)):
+            try:
+                res = cv2.matchTemplate(img_gray[np.maximum(int(bbox[j, 0, 1]), 0):int(bbox[j, 2, 1]),
+                                        np.maximum(int(bbox[j, 0, 0]), 0):int(bbox[j, 2, 0])].astype(np.uint8),
+                                        templates[i], cv2.TM_CCOEFF_NORMED)
+            except:
+                res = 0
+            loc = np.where(res > 0.5)[0]
+            sizes[i][j] = loc.size
+    matched_bbox = np.argmax(np.sum(sizes, 0))
+    if np.max(np.sum(sizes, 0)) > 100:
+        template_matched = True
+    return template_matched, bbox[matched_bbox]
+
+
+def template_matching_si(image, bbox, templates, main_template):
+    template_matched = False
+    sizes = np.zeros((len(templates), len(bbox)))
+    for i, template in enumerate(templates):
+        template = cv2.resize(template, (224, 224))
+        template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
+        template = Image.fromarray(template)
+        t_img = Variable(normalize(to_tensor(scaler(template))).unsqueeze(0)).cuda()
+        t_h = model(t_img)
+        for j in range(len(bbox)):
+            try:
+                im_bbox = image[np.maximum(int(bbox[j, 0, 1]), 0):int(bbox[j, 2, 1]), np.maximum(int(bbox[j, 0, 0]), 0):int(bbox[j, 2, 0])]
+                im_bbox = cv2.resize(im_bbox, (224, 224))
+                im_bbox = cv2.cvtColor(im_bbox, cv2.COLOR_BGR2RGB)
+                im_bbox = Image.fromarray(im_bbox)
+                im_bbox = Variable(normalize(to_tensor(scaler(im_bbox))).unsqueeze(0)).cuda()
+                image_h = model(im_bbox)
+                sizes[i][j] = cosine_similarity(np.reshape(image_h.cpu().detach().numpy(), [1, -1]), np.reshape(t_h.cpu().detach().numpy(), [1, -1]))[0][0]
+            except:
+                sizes[i][j] = 0
+    matched_bbox = np.argmax(np.max(sizes, 0))
+    if np.max(np.max(sizes, 0)) > 0.2:
+        template_matched = True
+
+    sizes = np.zeros(len(bbox))
+    template = cv2.resize(main_template, (224, 224))
+    template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
+    template = Image.fromarray(template)
+    t_img = Variable(normalize(to_tensor(scaler(template))).unsqueeze(0)).cuda()
+    t_h = model(t_img)
+    for j in range(len(bbox)):
+        try:
+            im_bbox = image[np.maximum(int(bbox[j, 0, 1]), 0):int(bbox[j, 2, 1]), np.maximum(int(bbox[j, 0, 0]), 0):int(bbox[j, 2, 0])]
+            im_bbox = cv2.resize(im_bbox, (224, 224))
+            im_bbox = cv2.cvtColor(im_bbox, cv2.COLOR_BGR2RGB)
+            im_bbox = Image.fromarray(im_bbox)
+            im_bbox = Variable(normalize(to_tensor(scaler(im_bbox))).unsqueeze(0)).cuda()
+            image_h = model(im_bbox)
+            sizes[j] = cosine_similarity(np.reshape(image_h.cpu().detach().numpy(), [1, -1]), np.reshape(t_h.cpu().detach().numpy(), [1, -1]))[0][0]
+        except:
+            sizes[j] = 0
+    return template_matched, bbox[matched_bbox], matched_bbox, np.max(sizes)
+
+
+def get_img(args, img_path):
+    img = mpimg.imread(args.data_folder + 'image/' + img_path + '.jpg')
+    return img
+
+
+def get_surfacenormal(args, img_path):
+    sf = imgproc.loadImage(args.data_folder + 'surface_normal/' + img_path + '_sf.jpg')
+    sf = sf/255.
+    return sf
+
+
+def get_bbox(args, img_path):
+    with open(args.data_folder + 'det/' + img_path + '.txt', "r") as det_file:
+        bbox = det_file.readlines()
+    return bbox
+
+
+def get_depth(args, img_path):
+    depth = imgproc.loadImage(args.data_folder + 'depth/' + img_path + '_depth.jpg')
+    depth = depth / 255.
+    return depth
