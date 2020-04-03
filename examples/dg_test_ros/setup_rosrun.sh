afdfbff0077e84558e1d3c029442616ae9ebe0b2
ccsmm78 <ccsmm@etri.re.kr>
2020-04-03 22:39:25 +0900

    [dg_test_ros] The environment setup required for rosrun builds was made convenient.   New features of examples/dg_test_ros/setup_rosrun.sh :           - Automatically reflect user's project path in CMakeLists.txt (from .CMakeLists.txt)           - Automatically create user's catkin workspace : ~/catkin_ws_deepguier           - Automatically reflect user's project path in examples/dg_test_ros/src/dg_test.cpp (from .dg_test.cpp)           - Automaticaaly insert devel/setup.bash in dg_run.sh (from .dg_run.sh)

--- examples/dg_test_ros/setup_rosrun.sh
+++ examples/dg_test_ros/setup_rosrun.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 ##Current Directory : dgdir/examples/dg_test_ros
+
+catkin_ws="catkin_ws_deepguider"
+CWD=`pwd`
+
+## Create catkin workspace
+if [ ! -z ~/$catkin_ws ];then
+    source /opt/ros/kinetic/setup.bash
+    mkdir -p ~/$catkin_ws/src
+    cd ~/$catkin_ws/src
+    catkin_init_workspace
+    cd ..
+    catkin_make
+	cd $CWD
+fi
+
+## Set rosdir
+cd ~/$catkin_ws;rosdir=`pwd`;cd $CWD
+#rosdir="/work/deepguider"  # set ros workspace directory here.
+cp .dg_run.sh dg_run.sh
+grep setup dg_run.sh || echo "source $rosdir/devel/setup.bash" | cat - dg_run.sh > .tmp && mv .tmp dg_run.sh
+chmod +x dg_run.sh
+
+## 
+cd $CWD
 cd ../../
 dgdir=`pwd`
-rosdir="/work/dg_ros"  # set ros workspace directory here.
 
 if [ ! -d $rosdir ]; then
     echo "You have to set rosdir first.";
@@ -17,3 +40,7 @@ ln -sf $dgdir/bin/data $rosdir/
 cp $dgdir/examples/dg_test_ros/dg_run.sh $rosdir/
 cp $dgdir/examples/dg_test_ros/dg_databag.sh $rosdir/
 
+## change path of python source in dg_test.cpp
+cd $CWD
+sed -e "s|\/work\/deepguider|$dgdir|g" .CMakeLists.txt > CMakeLists.txt
+sed -e "s|\/work\/deepguider|$dgdir|g" src/.dg_test.cpp > src/dg_test.cpp
