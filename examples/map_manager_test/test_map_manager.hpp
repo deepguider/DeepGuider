4b9ab8fc1736e230d7b9b937ab64271426e98a5b
zeozeo7142 <zeozeo7142@gmail.com>
2020-04-07 13:48:58 +0900

    [MapManager] Modify the test code for the new version, Topological Map Server v0.3.

--- examples/map_manager_test/test_map_manager.hpp
+++ examples/map_manager_test/test_map_manager.hpp
@@ -23,17 +23,17 @@ int testSimpleMapManager()
 	if (ok)
 	{
 		VVS_CHECK_EQUL(manager.getPath().pts.size(), 37);
-		VVS_CHECK_EQUL(manager.getMap().nodes.size(), 195);
+		VVS_CHECK_EQUL(manager.getMap().nodes.size(), 201);
 	}
 
     // Get the map
 	dg::Map map;
-	ok = manager.getMap(559542564800095, 700, map);
+	ok = manager.getMap(559542565000236, 700, map);
 	if (ok)
-		VVS_CHECK_EQUL(map.nodes.size(), 804);
+		VVS_CHECK_EQUL(map.nodes.size(), 747);
 	ok = manager.getMap(36.382967999999998, 127.37138150000001, 700, map);
 	if (ok)
-		VVS_CHECK_EQUL(map.nodes.size(), 795);// 36.384063, 127.374733, 650.0));//
+		VVS_CHECK_EQUL(map.nodes.size(), 799);// 36.384063, 127.374733, 650.0));//
 
 	// Get the path
 	ok = manager.getPath("test_simple_path.json", path);
@@ -41,20 +41,20 @@ int testSimpleMapManager()
 		VVS_CHECK_EQUL(path.pts.size(), 37);
 	
 	// Find the node & edge
-	dg::Node* findNode = manager.getMap().findNode(559542564800095);
-	VVS_CHECK_EQUL(findNode->id, 559542564800095);
-	dg::Edge* findEdge = manager.getMap().findEdge(559542564800095, 559542564800098);// 559562564900154, 559562564900155);
-	VVS_CHECK_EQUL(findEdge->length, 34.39032729690151);// 13.370689140764002);
-	VVS_CHECK_EQUL((manager.getMap().findEdge(findEdge->id))->length, 34.39032729690151);// 13.370689140764002);
+	dg::Node* findNode = manager.getMap().findNode(559542565000236);
+	VVS_CHECK_EQUL(findNode->id, 559542565000236);
+	dg::Edge* findEdge = manager.getMap().findEdge(559542565000236, 559542565000238);// 559562564900154, 559562564900155);
+	VVS_CHECK_EQUL(findEdge->length, 31.267266147580031);// 13.370689140764002);
+	VVS_CHECK_EQUL((manager.getMap().findEdge(findEdge->id))->length, 31.267266147580031);// 13.370689140764002);
 
 	// Get the POI
 	std::list<dg::POI> poi_list;
 	ok = manager.getPOI(36.384063, 127.374733, 650.0, poi_list);
 	if (ok)
 		VVS_CHECK_EQUL(poi_list.size(), 613);	// 36.382057170000003, 127.36764620000000, 10000.0, ip).size(), 8815);
-	ok = manager.getPOI(559542564800095, 500.0, poi_list);
+	ok = manager.getPOI(559542565000236, 500.0, poi_list);
 	if (ok)
-		VVS_CHECK_EQUL(poi_list.size(), 212);
+		VVS_CHECK_EQUL(poi_list.size(), 86);
 	dg::POI poi;
 	ok = manager.getPOI(16099168, poi);
 	if (ok)
@@ -66,9 +66,9 @@ int testSimpleMapManager()
 	ok = manager.getStreetView(36.384063, 127.374733, 650.0, sv_list);
 	if (ok)
 		VVS_CHECK_EQUL(sv_list.size(), 2144);	// 36.382057170000003, 127.36764620000000, 10000.0, ip).size(), 36607);
-	ok = manager.getStreetView(559542564800095, 500.0, sv_list);
+	ok = manager.getStreetView(559542565000236, 500.0, sv_list);
 	if (ok)
-		VVS_CHECK_EQUL(sv_list.size(), 662);
+		VVS_CHECK_EQUL(sv_list.size(), 630);
 	dg::StreetView sv;
 	ok = manager.getStreetView(32364501511, sv);
 	if (ok)
