c74791c1bb53a3d0932d8ff8e0ad62963d5e4297
minnjung <minjung.kim1994@gmail.com>
2020-04-09 16:42:40 +0900

    [poi_recog] typo

--- src/logo_recog/test.py
+++ src/logo_recog/test.py
@@ -15,7 +15,7 @@ from utils import construct_DB, load_features, pad_image, load_extractor_model, 
 
 
 def main(args):
-    
+
     yolo = YOLO(**{'model_path': args.model_path,
                    'anchors_path': args.anchors,
                    'classes_path': args.yolo_classes_path,
@@ -23,67 +23,50 @@ def main(args):
                    'gpu_num': args.gpu_num,
                    'model_image_size': (416, 416),
                    })
-    
+
     if args.mode.lower() == 'detect':
         cat_list = list(Path(args.input_path).iterdir())
-        
-        roi_path = 'kr_brandROIs'
-        if not os.path.exists(roi_path):
-            os.mkdir(roi_path)
-        
+
         elapsed_t = 0
         img_cnt = 0
-        for cat in tqdm(cat_list):
-            base = os.path.basename(cat)
+        for cat in cat_list:
             img_list = list(Path(cat).iterdir())
-            cat_cnt = 0
-            if not os.path.exists(os.path.join(roi_path, base)):
-                os.mkdir(os.path.join(roi_path, base))
-                
             for img_path in img_list:
-                pred, _, t = detect_logo_only(str(img_path), yolo, 
-                                 save_img=False, save_img_path=args.result_path)
-                img = cv2.imread(str(img_path))
-                if img is not None:
-                    for p in pred:
-                        x1 = p[0]
-                        y1 = p[1]
-                        x2 = p[2]
-                        y2 = p[3]
-                        roi = img[y1:y2, x1:x2]
-
-                        output_path = os.path.join(roi_path, base, base + str(cat_cnt) + '.jpg')
-                        cv2.imwrite(output_path, roi)
-                    
+                pred, _, t = detect_logo_only(str(img_path), yolo,
+                                 save_img=args.save_img, save_img_path=args.result_path)
+                for p in pred:
+                    x1 = p[0]
+                    y1 = p[1]
+                    x2 = p[2]
+                    y2 = p[3]
+                    roi = im[y1:y2, x1:x2]
                 if t == 0:
                     continue
                 elapsed_t += t
                 img_cnt += 1
-                cat_cnt += 1
-                
         print('\nDetecting Logos from images completed! It tooks {:.3f} FPS'.format(img_cnt/elapsed_t))
-    
+
     elif args.mode.lower() == 'recog':
         model_preproc, params = initialize(yolo, args.recog_model, args.DB_path)
         img_list = list(Path(args.input_path).iterdir())
-        
+
         elapsed_t = 0
         img_cnt = 0
         for img_path in img_list:
-            _, _, t = detect_and_match(str(img_path), model_preproc, params, 
+            _, _, t = detect_and_match(str(img_path), model_preproc, params,
                              save_img=args.save_img, save_img_path=args.result_path)
             if t == 0:
                 continue
             elapsed_t += t
             img_cnt += 1
         print('\nDetecting and Recognizing Logos from images completed! It tooks {:.3f} FPS'.format(img_cnt/elapsed_t))
-        
+
     else: # args.model == 'DB'
         construct_DB(args.DB_list, args.recog_model, args.DB_path)
-    
+
     print("Done...!")
 
-    
+
 def initialize(yolo, model_name, DB_path):
     print("\n\nInitialization in progress...!\n")
     start = time.time()
@@ -96,12 +79,12 @@ def initialize(yolo, model_name, DB_path):
     # load pre-trained recognition model
     model, preprocessed, input_shape = load_extractor_model(model_name)
     my_preprocess = lambda x: preprocessed(pad_image(x, input_shape))
-    
+
     input_feat = extract_features(img_input, model, my_preprocess)
     sim_cutoff, (bins, cdf_list) = similarity_cutoff(input_feat, features, 0.95)
-    
+
     print("Done...! It tooks {:.3f} mins".format((time.time() - start)/60))
-    
+
     return (yolo, model, my_preprocess), (input_feat, sim_cutoff, bins, cdf_list, input_labels)
 
 
@@ -119,17 +102,17 @@ if __name__ == '__main__':
     parser.add_argument('--DB_list', type=str,
             default='./data/preprocessed/DB_list.txt',
             help = 'Path to load the list of training dataset with labels.')
-    parser.add_argument('--model_path', type=str, 
+    parser.add_argument('--model_path', type=str,
             default='./model/keras_yolo3/logs/0001/trained_weights_final.h5',
             #default='./model/keras_yolo3/model_data/yolo_weights_logos.h5',
             help = 'Path to Yolo model weight file')
-    parser.add_argument('--anchors', type=str, 
+    parser.add_argument('--anchors', type=str,
             default='./model/keras_yolo3/model_data/yolo_anchors.txt',
             help = 'Path to Yolo anchors')
-    parser.add_argument('--yolo_classes_path', type=str, 
+    parser.add_argument('--yolo_classes_path', type=str,
             default='./data/preprocessed/openset_classes.txt',
             help = 'Path to Yolo class specifications')
-    parser.add_argument('--classes_path', type=str, 
+    parser.add_argument('--classes_path', type=str,
             default='./data/preprocessed/trained_brands.pkl',
             help = 'Path to load the brandROIs with labels')
     parser.add_argument('--recog_model', type=str, default='InceptionV3',
@@ -141,5 +124,5 @@ if __name__ == '__main__':
     parser.add_argument('--confidence', type=float, dest='score', default=0.05,
             help = 'Yolo object confidence threshold above which to show predictions')
     args = parser.parse_args()
-    
+
     main(args)
