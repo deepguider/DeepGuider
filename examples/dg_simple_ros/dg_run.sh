317de31f34bf832ab3fcada6d20dfbb7e230c2cc
roricljy <roricljy@gmail.com>
2020-07-08 13:41:20 +0900

    [dg_ros] fix node name in example script (dg_run.sh)

--- examples/dg_simple_ros/dg_run.sh
+++ examples/dg_simple_ros/dg_run.sh
@@ -2,4 +2,4 @@ gnome-terminal -e roscore
 sleep 2s    # wait until roscore is ready
 gnome-terminal -e "rosbag play --pause -s 500 -r 10 ./recordings/2020-02-19-15-01-53.bag"
 echo "Press [Space] to start rosbag play on the gnome terminal"
-rosrun dg_ros dg_ros_node
+rosrun dg_simple_ros dg_simple_ros
