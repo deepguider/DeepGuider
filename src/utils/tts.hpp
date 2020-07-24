825c039e69b2eff7eb8d88302947485a23041a2b
roricljy <roricljy@gmail.com>
2020-07-24 10:48:44 +0900

    [general] exclude 'tts' from Windows build` because it only works on Linux currently.

--- src/utils/tts.hpp
+++ src/utils/tts.hpp
@@ -8,18 +8,22 @@
 #ifndef __DG_TTS__
 #define __DG_TTS__
 
+#ifndef _WIN32
 #include <bits/stdc++.h> 
+#endif	// _WIN32
 
 namespace dg
 {
 
 int tts(std::string sentence)
 {
+#ifndef _WIN32
 	// gtts-cli '안녕하세요' --output tmp.mp3;ffplay -nodisp -autoexit tmp.mp3 >/dev/null 2>&1
 	std::string str = "gtts-cli '";
 	str = str + sentence + "' --output tmp.mp3;ffplay -nodisp -autoexit tmp.mp3 >/dev/null 2>&1";
 	const char *command = str.c_str(); 
 	system(command);
+#endif	// _WIN32
 	return 0;
 }
 
