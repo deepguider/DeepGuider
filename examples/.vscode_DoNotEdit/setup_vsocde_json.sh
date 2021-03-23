05aacf39bf6f12876d924203baf8ea4adaddebaf
ccsmm78 <ccsmm78@gmail.com>
2021-03-23 11:31:25 +0900

    Added initial configuration of .vscode. setup_vscode_json.sh which is executed by setup_all.sh automatically will configure the json files to suit your path environment.

--- examples/.vscode_DoNotEdit/setup_vsocde_json.sh
+++ examples/.vscode_DoNotEdit/setup_vsocde_json.sh
@@ -6,9 +6,17 @@ cd ../../
 topdir=`pwd`
 cd $CWD
 
+python_include=`python3 -c "from sysconfig import get_paths as gp; print(gp()['include'])"`
+
 mkdir -p ../.vscode
 cp ./*.json ../.vscode/.
 
-sed -e "s|/work/deepguider|${topdir}|g" c_cpp_properties.json > ../.vscode/c_cpp_properties.json
+if [ -e ${topdir} ]; then
+	sed -i "s|/work/deepguider|${topdir}|g" ../.vscode/c_cpp_properties.json
+fi
+
+if [ -e ${python_include} ]; then
+	sed -i "s|/usr/include/python3.6m|${python_include}|g" ../.vscode/c_cpp_properties.json
+fi
 
 cd $CWD   # return to initial directory
