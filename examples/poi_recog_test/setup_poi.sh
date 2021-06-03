e9e202bdee1b96956018075a951f6289524f83e9
roricljy <roricljy@gmail.com>
2021-06-03 15:29:32 +0900

    [poi_recog] deleted (it is no longer valid. it was replaced by logo_recog)

--- examples/poi_recog_test/setup_poi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-##Current Directory : deepguider/examples/poi_recog_test
-
-cd ../../bin
-ln -sf ../src/poi_recog/data_poi
-ln -sf ../src/poi_recog/model_poi
-cd ../examples/poi_recog_test   # return to initial directory
--- src/dg_poi_recog.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __DG_POI_RECOG__
-#define __DG_POI_RECOG__
-
-#include "poi_recog/poi_recognizer.hpp"
-
-#endif // End of '__DG_POI_RECOG__'
