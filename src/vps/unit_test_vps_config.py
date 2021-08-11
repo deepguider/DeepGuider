2d98235f80b4719541a70cc372f7c80dfc3b639f
ccsmm78 <ccsmm78@gmail.com>
2021-08-11 10:10:28 +0900

    Add unit test python scripts for vps and lrpose_recog

---
+++ src/vps/unit_test_vps_config.py
@@ -0,0 +1,7 @@
+date_idx = 0
+ip = "129.254.81.204"
+indir = "/home/ccsmm/data1/workdir/DB_Repo2/DeepGuider_system_testset/ETRI"
+input_reference_ext = "*.avi"
+begin_skip_frame = 3000
+out_postfix = "vps_test.csv"
+which_gpu = 3
