e6e42d90c37e2adcdf6653bffb414f8033c768b7
ccsmm78 <ccsmm78@gmail.com>
2021-08-13 15:06:55 +0900

    Modify unit_test for vps and lrpose_recog :  - It can detect avi and mkv  - timestamp in csv has 10.3f format from 10.2f

--- src/lrpose_recog/run_unit_test.sh
+++ src/lrpose_recog/run_unit_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 source ~/.virtualenvs/dg_venv3.6/bin/activate
 
 python unit_test_lrpose_recog.py
--- src/lrpose_recog/unit_test_lrpose_recog_config.py
+++ src/lrpose_recog/unit_test_lrpose_recog_config.py
@@ -1,7 +1,15 @@
-date_idx = 3
-ip = "129.254.81.204"
-indir = "/home/ccsmm/data1/workdir/DB_Repo2/DeepGuider_system_testset/ETRI"
-input_reference_ext = "*.avi"
+dataset_region="ETRI"
+date_idx = 0
 begin_skip_frame = 0
+ip = "129.254.81.204"
+if dataset_region == "ETRI":
+    indir = "/home/ccsmm/data1/workdir/DB_Repo2/DeepGuider_system_testset/ETRI"
+    input_reference_ext = "*.avi"
+elif dataset_region == "COEX":
+    indir = "/home/ccsmm/data1/workdir/DB_Repo2/DeepGuider_system_testset/COEX"
+    input_reference_ext = "*.mkv"
+else:
+    indir = "/home/ccsmm/data1/workdir/DB_Repo2/DeepGuider_system_testset/ETRI"
+    input_reference_ext = "*.avi"
 out_postfix = "vps_lr.csv"
 which_gpu = 1
