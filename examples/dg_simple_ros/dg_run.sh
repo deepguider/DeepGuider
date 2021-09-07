77bbe7fc208813a7bca79e6a943ad7fc7c033c84
ccsmm78@gmail.com <ccsmm78@gmail.com>
2021-09-07 14:34:54 +0900

    modified ros run script

--- examples/dg_simple_ros/dg_run.sh
+++ examples/dg_simple_ros/dg_run.sh
@@ -4,12 +4,14 @@ source /opt/ros/melodic/setup.bash
 catkin_make
 source devel/setup.bash
 
+## Start roscore
 pid=`pgrep roscore`
 if [ ! -n pid ];then  # If process is not running.
     gnome-terminal -- roscore
     sleep 2s    # wait until roscore is ready
 fi
 
+## Start rosbag play
 pid=`pgrep rosbag`
 if [ -n "${pid}" ];then  # If process is running.
     kill -9 ${pid}
@@ -17,10 +19,16 @@ fi
 gnome-terminal -- rosbag play --pause -s 500 -r 10 ./recordings/2020-02-19-15-01-53.bag
 echo "Press [Space] to start rosbag play on the gnome terminal"
 
-pid=`pgrep rosbag`
+## Start dg_ocr
+pid=`pgrep dg_ocr`
 if [ -n "${pid}" ];then  # If process is running.
     kill -9 ${pid}
 fi
 gnome-terminal -- rosrun dg_simple_ros dg_ocr # run ocr_ros first in another console
 
+## Start dg_simple_ros
+pid=`pgrep dg_simple_ros`
+if [ -n "${pid}" ];then  # If process is running.
+    kill -9 ${pid}
+fi
 rosrun dg_simple_ros dg_simple_ros            # run dg_simple_ros
--- examples/dg_simple_ros/setup_rosrun.sh
+++ examples/dg_simple_ros/setup_rosrun.sh
@@ -27,6 +27,7 @@ fi
 mkdir -p $ROSDIR/src
 ln -sf $DGDIR/examples/dg_simple_ros $ROSDIR/src/
 ln -sf $DGDIR/bin/font $ROSDIR/
+ln -sf $DGDIR/bin/recordings $ROSDIR/
 ln -sf $DGDIR/bin/data $ROSDIR/
 ln -sf $DGDIR/bin/data_localizer $ROSDIR/
 ln -sf $DGDIR/src/vps/data_vps $ROSDIR/
