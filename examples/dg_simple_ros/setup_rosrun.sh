e5d722cee6330840429ac703b927480839257c60
ccsmm78@gmail.com <ccsmm78@gmail.com>
2021-09-07 11:53:39 +0900

    Modify setup script for your ros running environment. After git clone, run prepare_deepguider_bin.sh in which setup_rosrun.sh  will be executed. Remove opencv version requirement causing lib error in CMakeListsTemplate.txt

--- examples/dg_simple_ros/setup_rosrun.sh
+++ examples/dg_simple_ros/setup_rosrun.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 ## Set path of deepguider and ros workspace
-DGDIR="/home/ccsmm/tmp/DeepGuider"        # path of DeepGuider source root
-ROSDIR="/home/ccsmm/catkin_ws"           # path of ROS workspace
-
+#DGDIR="/home/ccsmm/tmp/DeepGuider"        # path of DeepGuider source root
+CWD=`pwd`
+cd ../../
+DGDIR=`pwd`
 ## Check if specified DeepGuider directory exists.
 if [ ! -d $DGDIR ]; then
     echo "Error: The specified DeepGuider directory '$DGDIR' doesn't exists." 
     echo "Modify it to valid path."
     exit
 fi
+cd $DGDIR
+
+#ROSDIR="/home/ccsmm/catkin_ws"           # path of ROS workspace
+ROSDIR="/home/${USER}/catkin_ws"         # path of ROS workspace
 
 ## Check if specified ROS workspace exists.
 if [ ! -d $ROSDIR ]; then
-    echo "Error: The specified ROS workspace '$ROSDIR' doesn't exists." 
+    echo "Warning: The specified ROS workspace '$ROSDIR' doesn't exists." 
     echo "Create it first or specify valid ROS workspace path."
-    exit
+	mkdir -p $ROSDIR
 fi
 
 ## Make symbolic links to ROS workspace
-mkdir -p ~/$ROSDIR/src
+mkdir -p $ROSDIR/src
 ln -sf $DGDIR/examples/dg_simple_ros $ROSDIR/src/
 ln -sf $DGDIR/bin/data $ROSDIR/
 ln -sf $DGDIR/bin/data_localizer $ROSDIR/
@@ -34,9 +39,14 @@ ln -sf $DGDIR/src/poi_recog/model_poi $ROSDIR/
 ## Copy machine-specific files from template to local
 cp $DGDIR/examples/dg_simple_ros/CMakeListsTemplate.txt $DGDIR/examples/dg_simple_ros/CMakeLists.txt
 cp $DGDIR/examples/dg_simple_ros/dg_ros.yml $ROSDIR/
+sed -i "s|/work/deepguider|$DGDIR|g" $DGDIR/examples/dg_simple_ros/CMakeLists.txt
+sed -i "s|/home/dgtest/deepguider|$DGDIR|g" $ROSDIR/dg_ros.yml
 
 ## Copy example shell scripts to ROS workspace
 cp $DGDIR/examples/dg_simple_ros/dg_run.sh $ROSDIR/
 cp $DGDIR/examples/dg_simple_ros/dg_databag.sh $ROSDIR/
 chmod a+x $ROSDIR/dg_run.sh
 chmod a+x $ROSDIR/dg_databag.sh
+
+echo "Run:"
+echo "     cd $ROSDIR;./dg_run.sh"
