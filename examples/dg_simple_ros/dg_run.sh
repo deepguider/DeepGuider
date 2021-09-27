0a3bd78da75af1830467510c4c2993528045d473
roricljy <roricljy@gmail.com>
2021-09-27 13:33:41 +0900

    [dg_ros] fix error in dg_run.sh

--- examples/dg_simple_ros/dg_run.sh
+++ examples/dg_simple_ros/dg_run.sh
@@ -6,11 +6,9 @@ source devel/setup.bash
 
 ## Start roscore
 pid=`pgrep roscore`
-if [ ! -n pid ];then  # If process is not running.
+if [ ! -n "${pid}" ];then  # If process is not running.
     gnome-terminal -- roscore
     sleep 2s    # wait until roscore is ready
-else
-	gnome-terminal -- roscore
 fi
 
 ## Start rosbag play
@@ -22,11 +20,11 @@ gnome-terminal -- rosbag play --pause -s 500 -r 10 ./recordings/2020-02-19-15-01
 echo "Press [Space] to start rosbag play on the gnome terminal"
 
 ## Start dg_ocr
-pid=`pgrep dg_ocr`
-if [ -n "${pid}" ];then  # If process is running.
-    kill -9 ${pid}
-fi
-gnome-terminal -- rosrun dg_simple_ros dg_ocr # run ocr_ros first in another console
+#pid=`pgrep dg_ocr`
+#if [ -n "${pid}" ];then  # If process is running.
+#    kill -9 ${pid}
+#fi
+#gnome-terminal -- rosrun dg_simple_ros dg_ocr # run ocr_ros first in another console
 
 ## Start dg_simple_ros
 pid=`pgrep dg_simple_ros`
