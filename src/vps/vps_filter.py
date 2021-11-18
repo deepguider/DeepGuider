4869b351885028c99842af3cbd74e63c17a110f7
ccsmm78 <ccsmm78@gmail.com>
2021-11-18 21:04:03 +0900

    [vps] Modified vps_filter.py. Checking utm_distance makes a good filtering results for vps output.

--- src/vps/vps_filter.py
+++ src/vps/vps_filter.py
@@ -9,19 +9,23 @@ from skimage.measure import LineModelND, ransac
 class vps_filter:
     def __init__(self):
         self.n_sample_count = 0
-        self.n_samples = 20  # filter size of points
-        self.n_samples_max = 100  # maximum filter size of points
+        self.n_samples = 40  # filter size of points for ransac
+        #self.n_mean_samples = 15  # filter size of points for average filter
+        self.n_samples_max = 100  # maximum filter size of points for ransac
+        self.n_samples_min = 10   # minimum filter size of points for ransac
         self.dims = 2
         self.utm_xys = np.zeros((self.n_samples_max, self.dims))
         self.utm_xys[:,0] = np.arange(self.n_samples_max)
         self.utm_xys[:,1] = np.arange(self.n_samples_max)
 
         if True:
-            self.ransacline_distance_threshold = 10 # noisy postion difference in meters
-            self.utm_distance_threshold = 7
+            #self.ransacline_distance_threshold = 15  # Distance between current point and estimated line
+            self.utm_distance_threshold = 5  # Distance between current point and average of points
+            #self.utm_differential_threshold = 30  # Differential in the previous ponits. If there is large disarity and not back to be small, then it means noisy data.
         else:  # Large number means disabling this filter
-            self.ransacline_distance_threshold = 100 # noisy postion difference in meters
-            self.utm_distance_threshold = 100
+            self.ransacline_distance_threshold = 500 # noisy postion difference in meters
+            self.utm_distance_threshold = 500
+            self.utm_differential_threshold = 500
 
     def get_samples_toy_example(self, n_samples=10, n_outliers_ratio=0.4):  # For toy example
         if n_samples <= 0:
@@ -56,7 +60,7 @@ class vps_filter:
         a = float(A)
         b = float(-1)
         c = float(B)
-        print("[vps] {}x + {}y + {} = 0".format(a, b, c))
+        #print("[vps] {}x + {}y + {} = 0".format(a, b, c))
         return a, b, c
 
     def shortest_distance(self, x1, y1, a, b, c):
@@ -83,24 +87,59 @@ class vps_filter:
 
     def check_valid(self, new_x, new_y):
         self.update_samples(new_x, new_y)
-        utm_xys = self.get_samples()
-        if len(utm_xys) < self.n_samples:
+        utm_xys = self.get_samples(self.n_samples)
+        if len(utm_xys) < self.n_samples_min:
+            return False
+        mean_xys = np.median(utm_xys, axis=0)
+        utm_distance = np.sqrt(np.sum((mean_xys - [new_x, new_y])**2))  # meter
+        ## Check 2 : distance average point and current point in the small window
+        if utm_distance < self.utm_distance_threshold:  # Check new point is near to mean of previous point.
+            return True
+        else:
+            #print("[vps] Filter out ==========================> distance to utm : {}".format(utm_distance))
+            return False
+
+    def check_valid_ori(self, new_x, new_y):
+        self.update_samples(new_x, new_y)
+        utm_xys = self.get_samples(self.n_samples)
+        if len(utm_xys) < self.n_samples_min:
             return False
         try:
-            ransac_model, inliers = ransac(utm_xys, LineModelND, min_samples=2, residual_threshold=1, max_trials=1000)
-            #ransac.fit(utm_xys[:,0].reshape(-1,1), utm_xys[:,1].reshape(-1,1))
+            ransac_model, inliers = ransac(utm_xys, LineModelND, min_samples=2, residual_threshold=0.5, max_trials=1000)
             a, b, c = self.line_function_from_ransac(ransac_model)
             d = self.shortest_distance(new_x, new_y, a, b, c)
+
+            ## Check1 : distance between line and current point in the large window
             if d <= self.ransacline_distance_threshold:  # Check new point is near to ransac line
-                mean_xys = np.median(utm_xys[inliers], axis=0)
-                utm_distance = np.sqrt(np.sum((mean_xys - [new_x, new_y])**2))  # meter
-                if utm_distance < self.utm_distance_threshold:  # Check new point is near to mean of previous point.
-                    return True
+                inliers_count = np.sum(inliers)
+                if self.n_mean_samples > inliers_count:
+                    n_mean_samples = inliers_count
                 else:
-                    #print("[vps] Filter out ===============> distance to utm : {}".format(utm_distance))
+                    n_mean_samples = self.n_mean_samples
+                mean_xys = np.median(utm_xys[inliers][(inliers_count-n_mean_samples):,:], axis=0)
+                utm_distance = np.sqrt(np.sum((mean_xys - [new_x, new_y])**2))  # meter
+                ## Check 2 : distance average point and current point in the small window
+                if utm_distance < self.utm_distance_threshold:  # Check new point is near to mean of previous point.
+                    ## Check 3 : differential in the large window (noise to change to anohter road)
+                    differential1 = np.sqrt(np.sum(np.sum(np.diff(utm_xys, axis=0), axis=0)**2))
+                    #differential2 = np.sqrt(np.sum(np.sum(np.diff(np.diff(utm_xys, axis=0), axis=0), axis=0)**2))
+                    differential = differential1
+                    if differential < self.utm_differential_threshold:  # Suppress large disparity in the moving window.
+                        print("[vps] Pass =========>d : {}, utm_distance : {},  differential : {}".format(d, utm_distance, differential), len(utm_xys))
+                        return True
+                    else:
+                        print("[vps] Filter out =========================> differential : {}".format(differential))
+                        print("[vps] Filter out =========================> differential : {}".format(differential))
+                        print("[vps] Filter out =========================> differential : {}".format(differential))
+                        print("[vps] Filter out =========================> differential : {}".format(differential))
+                        print("[vps] Filter out =========================> differential : {}".format(differential))
+                        print("[vps] Filter out =========================> differential : {}".format(differential))
+                        return False
+                else:
+                    #print("[vps] Filter out ==========================> distance to utm : {}".format(utm_distance))
                     return False
             else:
-                #print("[vps] Filter out ===============> distance to ransac line : {}".format(d))
+                #print("[vps] Filter out ============================> distance to ransac line : {}".format(d))
                 return False
         except:
             return False
