e8e8dc3cdeac605fbe5476597aa28a0bd12776d8
Minjung Kim <minjung.kim1994@gmail.com>
2022-03-17 23:48:31 +0900

    [door_detect} initial commit

---
+++ src/door_detect/test_wrapper.py
@@ -0,0 +1,27 @@
+import os
+import time
+import glob
+from door_detector import DoorDetector
+from tqdm import tqdm
+
+imagepath = 'demo_data'
+
+start = time.time()
+
+test = DoorDetector()
+test.initialize()
+
+imgs = glob.glob(os.path.join(imagepath, '*.jpg'))
+imgs.sort()
+
+with open('pred_list.txt', 'w') as f:
+    for img in tqdm(imgs):
+        pred, timestamp = test.apply(img, start, True)
+        for p in pred:
+            f.write(p[1]+' ')
+        #base = os.path.basename(img)
+        f.write(img+'\n')
+        #captured = float(base.split('_')[1].split('.')[0]) / 60
+        #f.write(base + ' ' + f'{captured:.2f}\n')
+    
+print('\nDone! It tooks {:.2f} sec.\n'.format((time.time() - start)))
