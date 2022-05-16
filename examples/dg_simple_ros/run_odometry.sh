d033b6d19e5df743ca08896cdf7cb38b1b343c89
roricljy <roricljy@gmail.com>
2022-05-16 13:22:14 +0900

    [dg_ros] add odometry test script

---
+++ examples/dg_simple_ros/run_odometry.sh
@@ -0,0 +1,19 @@
+source devel/setup.bash
+catkin_make
+
+## Start roscore
+pid=`pgrep roscore`
+if [ ! -n "${pid}" ];then  # If process is not running.
+    gnome-terminal --tab -- roscore
+    sleep 2s    # wait until roscore is ready
+fi
+
+## Start dg_simple_ros package (working directory: devel/lib/dg_simple_ros/)
+pid=`pgrep -f dg_odometry`
+if [ -n "${pid}" ];then  # If process is running.
+    kill -9 ${pid}
+fi
+
+gnome-terminal --tab -- rosrun dg_simple_ros dg_odometry
+#gnome-terminal --tab -- rostopic echo /dg_odometry/pose
+
