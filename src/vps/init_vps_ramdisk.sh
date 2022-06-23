6d2a32939d1f72465d380f975f7788acd9e3cef2
ccsmm78@gmail.com <ccsmm78@gmail.com>
2022-06-23 18:08:06 +0900

    [vps] Added 777 permision to /mnt/ramdisk in catkin_ws/init_vps_ramdisk.sh

--- src/vps/init_vps_ramdisk.sh
+++ src/vps/init_vps_ramdisk.sh
@@ -7,6 +7,7 @@ ramdisk="/mnt/ramdisk"
 if [ ! -d $ramdisk ];then
 	sudo mkdir -p ${ramdisk}
 	sudo mount -t tmpfs -o size=4G tmpfs ${ramdisk}
+	sudo chmod 777 ${ramdisk} -R
 	echo "You can mount ramdisk automatically by adding following line at the end of the /etc/fstab"
 	echo "  none /mnt/ramdisk tmpfs defaults,size=4G 0 0"
 fi
