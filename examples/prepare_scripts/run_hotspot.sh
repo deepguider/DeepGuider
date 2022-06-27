875da669c6095e6ed784abb6f4e509fab1f6acd7
ccsmm78@gmail.com <ccsmm78@gmail.com>
2022-06-28 01:51:43 +0900

    Modified run_hotspot.sh

--- examples/prepare_scripts/run_hotspot.sh
+++ examples/prepare_scripts/run_hotspot.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
+#ccsmm@etri.re.kr
 
 # Refer https://computingforgeeks.com/create-wi-fi-hotspot-on-ubuntu-debian-fedora-centos-arch/
+#       https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=ijoos&logNo=221804461551
 
 IFNAME="wlo1"  # you can find with "$ip link show"
-CON_NAME="dg_hotspot"
+CON_NAME="Hotspot"  # It will be saved in /etc/NetworkManager/system-connections as a file
+PW="dgtest1234"
 
-nmcli con add type wifi ifname $IFNAME con-name $CON_NAME autoconnect yes ssid $CON_NAME
-nmcli con modify $CON_NAME 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
-nmcli con modify $CON_NAME wifi-sec.key-mgmt wpa-psk
-nmcli con modify $CON_NAME wifi-sec.psk "dg12345678"
-nmcli con up $CON_NAME
+if [ $1 ];then
+	if [ "$1" = "on" ];then
+		nmcli con up $CON_NAME
+	elif [ "$1" = "off" ];then
+		nmcli con down $CON_NAME
+	elif [ "$1" = "new" ];then
+		# It will be saved in /etc/NetworkManager/system-connections as a ${CON_NAME} file
+		nmcli con add type wifi ifname $IFNAME con-name $CON_NAME autoconnect yes ip4 192.168.0.10/24 ssid $CON_NAME
+		nmcli con modify $CON_NAME 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+		nmcli con modify $CON_NAME wifi-sec.key-mgmt wpa-psk
+		nmcli con modify $CON_NAME wifi-sec.psk "$PW"
+		nmcli con up $CON_NAME
+	else
+		echo "Usage: $0 [on/off]"
+	fi
+else
+	echo "Usage: $0 [on/off]"
+fi
+
+
+## List in /etc/NetworkManager/system-connections
+#nmcli connection
+
+## Delete $CON_NAME file in /etc/NetworkManager/system-connections
+#nmcli connection delete $CON_NAME
 
 ## Verify
 #nmcli connection show $CON_NAME
+
